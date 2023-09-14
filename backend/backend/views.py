88d5744f28a4769527d71103f311b0008177334a
Sir-1tudu <139034045+Sir-1tudu@users.noreply.github.com>
2023-09-14 15:24:10 +0530

    Add project files.

---
+++ backend/backend/views.py
@@ -0,0 +1,7 @@
+from django.http import JsonResponse
+from django.core import serializers
+from models import All_data
+
+def home(request):
+    all_data = 
+    return JsonResponse('data' = all_data)
