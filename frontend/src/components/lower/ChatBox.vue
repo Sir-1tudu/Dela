59a1b9d1fe587723d989410fb529fc8bbb118be1
Sir-1tudu <139034045+Sir-1tudu@users.noreply.github.com>
2023-09-27 05:46:25 +0530

    change the chatbox to the table format because it is easy to maintain and also quick code.

--- frontend/src/components/lower/ChatBox.vue
+++ frontend/src/components/lower/ChatBox.vue
@@ -4,15 +4,39 @@
         <ol class="list_container" v-for="i in users">
             <li class="list_cage">
                 <img src="" class="user_image_for_container" />
-                <p class="user_name_in_container">
-                    {{ i.name }}
-                    <pre class="date_and_time">{{ dateTime }}</pre>
-                </p>
+                <div class="user_detail_in_container">
+                    <p class="user_name_in_container">{{ i.name }}</p>
+                    <p class="date_and_time">{{ dateTime }}</p>
+                </div>
                 <p class="user_msg_count">{{ i.msg_count }}</p>
-                <div class="circle" v-bind="bgcolor"></div>
+                <div class="circle"></div>
             </li>
         </ol>
     </div>
+
+    <table>
+        <tr>
+            <th>image</th>
+            <th>name</th>
+            <th>msg</th>
+            <th>sts</th>
+            <th>drop down</th>
+        </tr>
+        <tr>
+            <td>img</td>
+            <td>shyam</td>
+            <td>3</td>
+            <td>online</td>
+            <td>go</td>
+        </tr>
+        <tr>
+            <td>img</td>
+            <td>shyam</td>
+            <td>3</td>
+            <td>online</td>
+            <td>go</td>
+        </tr>
+    </table>
 </template>
 
 <script>
@@ -21,18 +45,18 @@
             return {
                 users: [
                     { name: 'hari charan das', status_of_friend: false, msg_count: 20 },
-                    { name: 'ram sing', status_of_friend: true, msg_count: 40 },
+                    { name: 'ram sing su sfsds', status_of_friend: true, msg_count: 40 },
                     { name: 'bindo bando', status_of_friend: true, msg_count: 50 }
                 ],
                 dateTime: '20.09.23:4.50 AM',
-                bgcolor : status_color
+                //bgcolor : scolor
             }
         }
     }
 </script>
 
 <style scoped>
-    .chat_box{
+    .chat_box {
         border: 1px solid black;
         width: 20%;
     }
@@ -47,8 +71,9 @@
     /*list_cage class*/
     .list_cage{
         display: flex;
+        align-items:center;
         flex-direction: row;
-        gap: .4em;
+        gap: .9em;
     }
     /*image of the user in chat box*/
     .user_image_for_container {
@@ -56,20 +81,30 @@
         width: 2.5em;
         border: 1px solid black;
     }
-
+    /*user detail container */
+    .user_detail_in_container {
+        border: 1px solid black;
+    }
     /*user name in the chat box*/
     .user_name_in_container {
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        font-family: Arial, Helvetica, sans-serif;
-        font-family: 'Tempus Sans ITC';
-        font-family: Arial, Helvetica, sans-serif;
         font-family: 'Global Monospace';
         color: black;
-        font-size: .9em;
+        font-size: 1em;
+        position: relative;
+        top: .3em;
+        width: max-content;
     }
     /*date and time box for chat when the last time the msg is deliverd */
-    .date_and_time{
+    .date_and_time {
         font-family: 'Global Monospace';
+        position: relative;
+        bottom: .5em;
+        font-size: .7em;
+    }
+    /*user message count for the chat box */
+    .user_msg_count{
+        border:1px solid black;
+        padding: 0em 1em 0em 1em;
     }
     /*circle for show the user status wheater it is offline or online in the chat box */
     .circle {
